14ce5edb782a2e62e606b8cbf611c5e576d33199
ankit-pn <miankitsingh@gmail.com>
2021-05-26 14:48:44 +0530

    javascript added

---
+++ ix.js
@@ -0,0 +1,9 @@
+document.getElementById("jenet").addEventListener("click", function(){
+    document.getElementById("id1").scrollIntoView(true,{behavior: "smooth", block: "end", inline: "nearest"});
+});
+document.getElementById("codskill").addEventListener("click", function(){
+    document.getElementById("cont2").scrollIntoView(true,{behavior: "smooth", block: "end", inline: "nearest"});
+});
+document.getElementById("note").addEventListener("click", function(){
+    document.getElementById("why").scrollIntoView(true,{behavior: "smooth", block: "end", inline: "nearest"});
+});
